2489e2df1497bbf15ad005ca9fa24e96c1eab6ef
Jatin-Miglani <miglanijatin26@gmail.com>
2022-08-18 00:06:01 +0530

    first commit

---
+++ code1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int c;
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+    cout<<"Enter a number"<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<c;
+
+    
+    return 0;
+}
---
+++ code2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers seperated by space"<<"\n";
+    cin>>a>>b;
+    cout<<"you Entered "<<a<<" and "<<b;
+    return 0;
+}
